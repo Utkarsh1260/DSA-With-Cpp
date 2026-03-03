907ced9d18e0677027d2c5c03fd1b2d87a9a911b
Utkarsh1260 <utkarsh.cyber01@gmail.com>
2026-03-03 10:15:31 +0530

    comments added

--- dsa/cpp_stl/pair.cpp
+++ dsa/cpp_stl/pair.cpp
@@ -4,13 +4,15 @@ using namespace std;
 
 int main(){
 
+    //declaration type 1
     pair<int, int> p={1,2};
     cout<<p.first <<" & " <<p.second <<endl;
 
+     //declaration type 2
     pair<int, pair<int, int>> q={1,{2,3}};
     cout<<q.first <<" & " <<q.second.first <<" & " <<q.second.second;
 
-
+     //declaration type 3
     pair<int, int> arr[]={{1,2},{3,4},{5,6}};
     cout<<endl;
     cout<<arr[1].second;
