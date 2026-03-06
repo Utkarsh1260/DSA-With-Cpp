afb9ee1c0a9a40abf0133be272007262f354c785
Utkarsh1260 <utkarsh.cyber01@gmail.com>
2026-03-06 21:09:57 +0530

    changes in comments

--- dsa/basics_concepts/inbulit_functions_for_vector.cpp
+++ dsa/basics_concepts/inbulit_functions_for_vector.cpp
@@ -5,22 +5,22 @@ using namespace std;
 int main() {
     vector<int> v;
     
-    // push_back() - add elements to the end
+    // add elements to the end
     v.push_back(10);
     v.push_back(20);
     v.push_back(30);
     v.push_back(40);
     
-    // size() - get number of elements
-    cout << "Size of vector: " << v.size() << endl;
+    // tells the total number of elements in the vector
+    cout << "Size of vector v: " << v.size() << endl;
     
-    // front() - access first element
-    cout << "Front element: " << v.front() << endl;
+    //  access first element
+    cout << "Front element of vector v: " << v.front() << endl;
     
-    // back() - access last element
-    cout << "Back element: " << v.back() << endl;
+    //  access last element
+    cout << "Back element of vector v: " << v.back() << endl;
     
-    // at() - access element at specific index
+    //  access element at specific index
     cout << "Element at index 1: " << v.at(1) << endl;
     cout << "Element at index 2: " << v.at(2) << endl;
     
