93292bcd9828ec4acb2f74cfb7ed47970e05c315
Utkarsh1260 <utkarsh.cyber01@gmail.com>
2026-03-08 16:08:47 +0530

    count the digit

---
+++ dsa/solved_problems/digit_count.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+#include <cmath>
+
+int count(int n) {
+    if(n == 0) return 1;
+    return log10(n) + 1;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int result=count(n);
+    cout<<result;
+    return 0; 
+}
