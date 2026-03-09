aeb79c2f93d8113ea4aa117392b4d3710cbcc36b
Utkarsh1260 <utkarsh.cyber01@gmail.com>
2026-03-09 18:30:36 +0530

    copy of variable is passed to function

--- cpp/pass_by_value.cpp
+++ cpp/pass_by_value.cpp
@@ -12,7 +12,7 @@ int main()
  {
 
     int a=2, b=3;
-    int s=sum(a,b); // It is pass by value because we are passing the value of a and b as a copy to function sum and not the reference of a and b means any changes in a and b at function side will not affect the value of a and b at main function 
+    int s=sum(a,b); // It is pass by value because we are passing the value of a and b as a copy to function sum and not the reference of a and b which means any changes in a and b at function side will not affect the value of a and b in main function 
     cout<<s;
 
   return 0;
