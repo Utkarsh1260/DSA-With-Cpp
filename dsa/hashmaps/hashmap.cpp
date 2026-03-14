d61cb511b3d8666b3025b04785902f2f181919f0
Utkarsh1260 <utkarsh.cyber01@gmail.com>
2026-03-14 15:34:46 +0530

    Implement frequency counting using map with query support

---
+++ dsa/hashmaps/hashmap.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int arr[n];
+    map<int, int> m;
+    for(int i=0; i<n; i++){ 
+        cin>>arr[i];
+        m[arr[i]]++;     //hashmap -processing
+    }
+
+
+
+
+
+    //mapprint
+    // for(auto it: m) cout<<it.first<<"->"<<it.second<<endl;
+
+
+    //queries
+    int t;
+    cin>>t;
+
+    while(t--){
+        int f;
+        cin>>f;
+         cout<<m[f]<<endl;
+
+    }
+
+    return 0;
+}
