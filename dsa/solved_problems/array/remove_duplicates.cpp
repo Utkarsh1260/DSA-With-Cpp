7e6efd034290858cdbca76d664884305239d33e1
Utkarsh1260 <utkarsh.cyber01@gmail.com>
2026-03-21 12:09:09 +0530

    return only unique elements

--- dsa/solved_problems/array/remove_duplicates.cpp
+++ dsa/solved_problems/array/remove_duplicates.cpp
@@ -1,21 +1,16 @@
-// Program to remove dubplicates from the given array
+// Program to remove duplicate elements from the given vector and return it
+
 #include<iostream>
 #include<vector>
+#include<set>
 using namespace std;
 
 
-    int removeDuplicates(vector<int>& nums) {
-        int unique=1;
-        int i=1;
-        while(i<nums.size()){
-            if(nums[i]!=nums[i-1]){
-                 nums[unique]=nums[i];
-                 unique++;
-        }
-          i++;
-        }
-        return unique;
-    }
+vector<int> removeDuplicates(vector<int>& nums) {
+    set<int> st(nums.begin(), nums.end());   
+    vector<int> unique(st.begin(), st.end()); 
+    return unique;
+}
 
 
 int main(){
@@ -31,15 +26,38 @@ for(int i=1; i<=10; i++)
         cin>>x;
         arr.push_back(x);
     }
-    int k = removeDuplicates(arr);
-    cout <<"original size= "<<n<<", "<<"unique= "<<k<<endl;;
+    vector<int> unique_array = removeDuplicates(arr);
+    cout <<"original size= "<<n<<", "<<"unique= "<<unique_array.size()<<endl;
+
+    for(int i=0; i<unique_array.size(); i++){
+        cout<<unique_array[i]<<" ";
+    }
+    cout<<endl;
 }
 
     return 0;
 }
 
 
+
+/* Time and Space Complexity analysis
+
+--> Time Complexity
+since insertion in set takes O(log n) and vector insertion in vector takes O(n)
+so all cases TC  is O(n logn)
+
+
+--> Space Complexity-:
+Function → O(n) becasue set and vector is storing n unique elements
+Overall program → O(n)
+
+*/
+
+
+
+
 /* Test Cases (10) -:
+
 5
 1 1 2 2 3
 5
@@ -59,4 +77,6 @@ for(int i=1; i<=10; i++)
 6
 2 2 2 3 4 5
 6
-5 5 5 6 6 6 8 8 */
+5 5 5 6 6 6 
+
+*/
