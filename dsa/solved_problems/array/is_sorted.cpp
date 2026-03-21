12ad8af8234df9fbff31d67aaa54313a0c789cda
Utkarsh1260 <utkarsh.cyber01@gmail.com>
2026-03-21 11:00:34 +0530

    is given vector sorted or not

--- dsa/solved_problems/array/is_sorted.cpp
+++ dsa/solved_problems/array/is_sorted.cpp
@@ -1,3 +1,5 @@
+// Program to check whether the given vector is sorted or not
+
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -10,17 +12,71 @@ int isSorted(vector<int> &arr, int n){
 
 
 int main(){
+for(int i=1; i<=10; i++)
+{
+
+
     vector<int> arr;
     int n;
     cin>>n;
-    for(int i=0; i<n; i++) {
+    for(int i=0; i<n; i++) 
+    {
         int x;
         cin>>x;
-        arr.push_back(x);}
+        arr.push_back(x);
+    }
+
     int result=isSorted(arr, n);
-    cout<<result;
 
+    bool boolean = result;
+    cout << "array size is = " << n;
+    cout << ", array is sorted = " <<boolean<<" ("<<boolalpha << boolean<<")";
 
+    cout<<endl;
+}
 
     return 0;
 }
+
+/* Time and Space Complexity analysis
+
+--> Time Complexity
+
+Best case O(1)
+if vector is empty or have only one element Loop runs 0 or 1 time
+
+Avg and Worst case O(n)
+
+
+--> Space Complexity-:
+Function → O(1)
+Overall program → O(n)
+
+*/
+
+
+
+
+/* Test Cases(10)
+
+1
+5
+5
+1 2 3 4 5
+5
+5 4 3 2 1
+4
+7 7 7 7
+6
+1 2 2 3 4 4
+5
+1 2 5 3 4
+5
+1 2 3 5 4
+5
+-5 -3 -1 0 2
+5
+-2 -1 3 2 4
+0
+
+*/
